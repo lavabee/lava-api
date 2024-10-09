f64feb2344d52a95395a63a792a83a41ff340f32
mrdoa <mr.doan2012@gmail.com>
2024-10-09 15:15:02 +0700

    update

--- routes/api/v2.php
+++ routes/api/v2.php
@@ -1,7 +1,6 @@
 <?php
 use App\Http\Controllers\Api\V2\TaskController;
 use App\Http\Controllers\Api\V2\CompleteTaskController;
-use \App\Http\Controllers\Api\V2\TestController;
 
 Route::middleware("auth:sanctum")->prefix('v2')->group(function (){
 
@@ -10,7 +9,4 @@ Route::middleware("auth:sanctum")->prefix('v2')->group(function (){
 
 
 
-
-    //test
-    Route::post('xml2json',[TestController::class,'xmlToJson']);
 });
