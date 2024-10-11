fa1129f090a500079bcc9c067ad67a7fb12cf270
mrdoa <mr.doan2012@gmail.com>
2024-10-11 07:42:15 +0700

    update

--- routes/api/test.php
+++ routes/api/test.php
@@ -4,7 +4,7 @@ use \App\Http\Controllers\Api\V2\TestController;
 
 Route::middleware("auth:sanctum")->prefix('test')->group(function (){
 
-    Route::post('/',[TestController::class,'index']);
+    Route::post('index',[TestController::class,'index']);
 
     Route::post('xml2json',[TestController::class,'xmlToJson']);
 
