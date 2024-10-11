bf6bf43c1527e4a5acffb87f1e6ea36abb527556
mrdoa <mr.doan2012@gmail.com>
2024-10-11 07:47:59 +0700

    update

--- app/Http/Controllers/Api/V2/TestController.php
+++ app/Http/Controllers/Api/V2/TestController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TestController extends Controller
 {
     public function index(Request $request)
     {
+        if ($request->isMethod('post')) {
+            // Log headers
+            $headers = $request->headers->all();
+            Log::info('Request Headers: ', $headers);
+
+            // Log body
+            $body = $request->all();
+            Log::info('Request Body: ', $body);
+        }
         return response($request->all());
     }
 
