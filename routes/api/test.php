8ce24a6a4737b1a40928c5bf47554d7d57eb3963
Your Name <you@example.com>
2024-10-24 10:29:52 +0700

    'update'

--- routes/api/test.php
+++ routes/api/test.php
@@ -12,5 +12,14 @@ Route::middleware("auth:sanctum")->prefix('test')->group(function (){
 
     Route::post('map',[TestController::class,'mapGateway']);
 
+    Route::get('/logs', function () {
+        $path = storage_path('logs/laravel.log'); // Đường dẫn đến file log
+        if (File::exists($path)) {
+            $logs = File::get($path);
+            return nl2br($logs); // Chuyển đổi ký tự xuống dòng thành thẻ <br> để hiển thị đúng trên trình duyệt
+        } else {
+            return 'Log file does not exist.';
+        }
+    });
 
 });
